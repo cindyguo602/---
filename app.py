6a93015022248fa90a7cbe0606fd0ff6770499e0
cindyguo602 <62104314+cindyguo602@users.noreply.github.com>
2026-01-28 18:02:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ SHEET_NAME = 'work_log'
 SUMMARY_SHEET_NAME = 'daily_summary'
 BUDGET_LIMIT = 120000
 BASE_RATE = 500
-ADMIN_PASSWORD = "1234"
+ADMIN_PASSWORD = "345678"
 
 # --- 核心：取得台灣時間 ---
 def get_taiwan_now():
@@ -435,3 +435,4 @@ with t3:
                     time.sleep(2)
                     st.rerun()
                 else: st.error("❌ 時間格式錯誤！")
+
